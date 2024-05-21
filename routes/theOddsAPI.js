cba46bece6f8fbe65981a15ab7a6c254ea1519cb
Ryan Callebs <ryan.callebs@gmail.com>
2024-05-21 16:16:07 -0400

    remove lines

--- routes/theOddsAPI.js
+++ routes/theOddsAPI.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const apiCtrl = require("../controllers/theOddsAPI");
 
-// Define routes
 router.get("/sports", apiCtrl.getSports);
 router.get("/odds", apiCtrl.getOdds);
 router.get("/odds/nfl", apiCtrl.getNFLOdds);
